5fe4275693552f1e95cafaa2d4bbff7ca904fb13
NathanDeveloping <winger.nathan.contact@gmail.com>
2017-05-24 10:53:45 +0200

    changement route

--- index.php
+++ index.php
@@ -7,9 +7,11 @@ use Slim\Slim;
 $app = new Slim();
 
 $app->get('/', function() {
+    echo "Seshat API v1 <br />";
+    echo "Labels : /api/labels <br />\"";
 });
 
-$app->get('/api/labels)', function(){
+$app->get('/api/labels', function() {
     $c = new DataController();
     $c->getLabels();
 });
